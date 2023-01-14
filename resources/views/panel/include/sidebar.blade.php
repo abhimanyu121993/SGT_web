5b822368f87e399d87ef07e2401672d0e122261e
abhimanyu121993 <softpromani@gmail.com>
2023-01-15 01:37:01 +0530

    logo

--- resources/views/panel/include/sidebar.blade.php
+++ resources/views/panel/include/sidebar.blade.php
@@ -13,8 +13,8 @@
 
 <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light sidenav-active-square">
     <div class="brand-sidebar">
-        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="#"><img class="hide-on-med-and-down" src="{{ asset('app-assets/images/logo/sgtLogo.jpeg') }}" alt="materialize logo" />
-            <img class="show-on-medium-and-down hide-on-med-and-up" src="{{ asset('app-assets/images/logo/sgtLogo.jpeg') }}" alt="materialize logo" />
+        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="#"><img class="hide-on-med-and-down" src="{{ asset('app-assets/images/logo/sgtlogo.jpeg') }}" alt="materialize logo" />
+            <img class="show-on-medium-and-down hide-on-med-and-up" src="{{ asset('app-assets/images/logo/sgtlogo.jpeg') }}" alt="materialize logo" />
             {{-- <span class="logo-text hide-on-med-and-down"></span></a> --}}
             <a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a>
         </h1>
