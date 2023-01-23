8838eaeca706d3a3f59eb69c437d28d00adeaa03
KillerHeman <sharmahcool5@gmail.com>
2023-01-23 17:13:26 +0530

    first

--- app/Http/Controllers/admin/ProfileController.php
+++ app/Http/Controllers/admin/ProfileController.php
@@ -10,6 +10,7 @@ use App\Models\Country;
 use App\Models\TimeZone;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
 class ProfileController extends Controller
@@ -81,24 +82,27 @@ class ProfileController extends Controller
     {
         dd($request->all());
         try{
-            $result = AdminProfile::find($id)
-            ->update([
-                'first_name' => $request->first_name,
-                'last_name' => $request->last_name,
-                'mobileno' => $request->mobileno,
-                'dob' => $request->dob,
-                'gender' => $request->gender,
-                'country' => $request->country,
-                'state' => $request->state,
-                'city' => $request->city,
-                'timezone' => $request->timezone,
-                'address' => $request->address,
-            ]);
+            AdminProfile::find($id)
+                ->update([
+                    'first_name' => $request->first_name,
+                    'last_name' => $request->last_name,
+                    'mobileno' => $request->mobileno,
+                    'dob' => $request->dob,
+                    'gender' => $request->gender,
+                    'country' => $request->country,
+                    'state' => $request->state,
+                    'city' => $request->city,
+                    'timezone' => $request->timezone,
+                    'address' => $request->address,
+                ]);
 
+            Session::flash('success', 'User updated successfully');
+            return redirect()->back();
 
         }
         catch(Exception $ex){
            Helper::handleError($ex);
+           return redirect()->back();
         }
     }
 
