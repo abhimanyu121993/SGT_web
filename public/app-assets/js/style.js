031f4e13ce94a788384476b4ee978b9aa517a95f
KillerHeman <sharmahcool5@gmail.com>
2023-01-23 11:09:09 +0530

    hisu push

--- public/app-assets/js/style.js
+++ public/app-assets/js/style.js
@@ -1,9 +1,9 @@
 $("#country").change(function() {
     $(this).find("option:selected").each(function() {
         var optionValue = $(this).attr("value");
-        alert(optionValue);
-        var newurl = {{ route('get-states') }};
-        alert(newurl);
+        // alert(optionValue);
+        // var newurl = {{ route('get-states') }};
+        // alert(newurl);
         $.ajax({
             url: newurl,
             method: 'get',
