ab9b1b5c92cc9f25801498d854f8f80bc8233b54
abhimanyu121993 <softpromani@gmail.com>
2023-01-24 12:29:27 +0530

    role-permissionseeder

---
+++ database/seeders/CustomerPermissionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PermissionName;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class CustomerPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+       
+    }
+}
