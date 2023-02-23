36b6830f447b3f1ea21887acffe2215cfe75f098
sha855 <alamshahreyar23@gmail.com>
2023-02-23 19:38:51 +0530

    property view complete

--- resources/views/customer/property/view_property.blade.php
+++ resources/views/customer/property/view_property.blade.php
@@ -2,6 +2,9 @@
 @section('title', 'Property')
 @section('breadcrumb')
 
+<link href="//cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
 
     <div class="breadcrumbs-dark pb-0 pt-4" id="breadcrumbs-wrapper">
@@ -185,8 +188,7 @@
                  <th>Company</th>
                  <th>Start Date</th>
                  <th>Status</th>
-                 <th>Amount</th>
-                 <th>Action</th>
+                
               </tr>
            </thead>
            <tbody>
@@ -240,69 +242,320 @@
 
 
 
-          <a class="btn waves-effect newlightbutton waves-light breadcrumbs-btn right" href="#!" style="margin-right:70px;"><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black">Routes</span></a>
+      <a class="btn waves-effect newlightbutton waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black">Filter</span></a>
 
-        <a class="btn waves-effect newlightbutton waves-light breadcrumbs-btn right" href="#!" style="margin-right:30px;"><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">QR MAP</span></a>
+        <a class="btn waves-effect Xnewlightbutton waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Sort</span></a>
+
+        <br><br>
+
+
+      <div class="belowsideOption">
+
+       
+ 
+
+        <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Completed</span></a>
+       
+
+         <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Upcoming</span></a>
+
+
+
+          <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Missed</span></a>
+
+    
+
+           <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">In-progress</span></a>
+
+      </div>
+    <br><br>
+
+
+    <div class="newFloat">
+ 
+      <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black">Shifts</span></a>
+
+       <br>
+ 
+
+    <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Completed</span></a>
+
+     <br>
+       
+
+      <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Upcoming</span></a>
+  <br>
+
+
+    <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">Missed</span></a>
+  
+   <br>
+    
+
+    <a class="btn waves-effect waves-light breadcrumbs-btn right" href="#!" ><i class="material-icons hide-on-med-and-up">settings</i><span class="hide-on-small-onl" style="color:black;">In-progress</span></a>
+
+</div>
 
 
 
 
 
-    <div class="col s12 m12 l6">
-      <div class="ct-chart card z-depth-2 border-radius-6">
+
+    <div class="col s12 m12 l6 classessmonster">
+
+
+      <div class="ct-chart card z-depth-2 border-radius-6  bigsizemake">
         <div class="card-content">
-          <h4 class="card-title">Tristate chart</h4>
-          <div id="profit-tristate" class="center"></div>
+         
+<table id="example" class="display nowrap" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>63</td>
+        
+      
+            </tr>
+            <tr>
+                <td>Ashton Cox</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td>66</td>
+        
+     
+            </tr>
+            <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+        
+      
+            </tr>
+            <tr>
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>33</td>
+        
+      
+            </tr>
+            <tr>
+                <td>Brielle Williamson</td>
+                <td>Integration Specialist</td>
+                <td>New York</td>
+                <td>61</td>
+        
+      
+            </tr>
+            <tr>
+                <td>Herrod Chandler</td>
+                <td>Sales Assistant</td>
+                <td>San Francisco</td>
+                <td>59</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Rhona Davidson</td>
+                <td>Integration Specialist</td>
+                <td>Tokyo</td>
+                <td>55</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Colleen Hurst</td>
+                <td>Javascript Developer</td>
+                <td>San Francisco</td>
+                <td>39</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Sonya Frost</td>
+                <td>Software Engineer</td>
+                <td>Edinburgh</td>
+                <td>23</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Jena Gaines</td>
+                <td>Office Manager</td>
+                <td>London</td>
+                <td>30</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Quinn Flynn</td>
+                <td>Support Lead</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Charde Marshall</td>
+                <td>Regional Director</td>
+                <td>San Francisco</td>
+                <td>36</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Haley Kennedy</td>
+                <td>Senior Marketing Designer</td>
+                <td>London</td>
+                <td>43</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Tatyana Fitzpatrick</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>19</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Michael Silva</td>
+                <td>Marketing Designer</td>
+                <td>London</td>
+                <td>66</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Paul Byrd</td>
+                <td>Chief Financial Officer (CFO)</td>
+                <td>New York</td>
+                <td>64</td>
+      
+    
+            </tr>
+            <tr>
+                <td>Gloria Little</td>
+                <td>Systems Administrator</td>
+                <td>New York</td>
+           
+            </tr>
+          
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+    
+
+            </tr>
+        </tfoot>
+    </table>
+
+
+
+
+
+
+
+
+
+
         </div>
       </div>
     </div>
-    <div class="col s12 m12 l6">
+
+   
+   <div class="sidecard" style="display:grid;">
+
+    <div class="col s12 m12 l6 classesdino">
       <div class="ct-chart card z-depth-2 border-radius-6">
         <div class="card-content">
           <div class="row">
             <div class="col s12">
-              <h4 class="card-title">Bar Chart</h4>
+              <h4 class="card-title">Reports</h4>
             </div>
-            <div class="col s12">
-              <div id="bar-chart-sample" class="center"></div>
-            </div>
+           
+
+Share on Twitter
+Share via Email
+Download PDF
+In the pursuit of wisdom, executives may find themselves taking off their masks to become truly authentic and reflective leaders.
+“I cannot teach anyone anything, I can only make them think.” - Socrates
+
+The day after becoming the CEO of a company facing turbulent times, David had a dream. In it, while walking on a beach he discovered a bottle. On opening, a genie appeared offering him a wish in exchange for her freedom. Eschewing riches, fame or a long life, David opted for the one thing he knew he needed to help him guide his people in the best way possible. He chose the gift of wisdom.
+
+
+
+
+
+
+
+
           </div>
         </div>
       </div>
     </div>
-    <div class="col s12 m12 l6">
+
+
+
+    <div class="col s12 m12 l6 smallclassesdino">
       <div class="ct-chart card z-depth-2 border-radius-6">
         <div class="card-content">
-          <h4 class="card-title">Bar + line composite charts</h4>
+          <h4 class="card-title">Activity Log</h4>
           <div id="sales-compositebar" class="center"></div>
+
+
+          Share on Twitter
+Share via Email
+Download PDF
+In the pursuit of wisdom, executives may find themselves taking off their masks to become truly authentic and reflective leaders.
+“I cannot teach anyone anything, I can only make them think.” - Socrates
+
+The day after becoming the CEO of a company facing turbulent times, David had a dream. In it, while walking on a beach he discovered a bottle. On opening, a genie appeared offering him a wish in exchange for her freedom. Eschewing riches, fame or a long life, David opted for the one thing he knew he needed to help him guide his people in the best way possible. He chose the gift of wisdom.
         </div>
+
+
+
+
+
+
       </div>
     </div>
-    <div class="col s12 m12 l6">
-      <div class="ct-chart card z-depth-2 border-radius-6">
-        <div class="card-content">
-          <div class="row">
-            <div class="col s12">
-              <h4 class="card-title">Pie Chart</h4>
-            </div>
-            <div class="col s12">
-              <div id="pie-chart-sample" class="center"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+</div>
+
+
+
+
+
+
   </div>
 </div>
-<!-- START RIGHT SIDEBAR NAV -->
-            
-
-
- 
-    <!-- END: Page Main-->
 
 @endsection
 @section('script-area')
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
     <script src="{{asset('app-assets/vendors/data-tables/js/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('app-assets/vendors/data-tables/extensions/responsive/js/dataTables.responsive.min.js')}}"></script>
     <script src="{{asset('app-assets/js/search.js')}}"></script>
@@ -310,4 +563,16 @@
 
     <script src="{{asset('app-assets/vendors/sparkline/jquery.sparkline.min.js')}}"></script>
     <script src="{{asset('app-assets/vendors/chartjs/chart.min.js')}}"></script>
+
+    <script>
+$(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+
+    </script>
 @endsection
