90be4291975f2ad19b584733c9d88c308046165c
abhimanyu121993 <softpromani@gmail.com>
2023-02-28 11:36:45 +0530

    activity Log

--- app/Models/admin/Admin.php
+++ app/Models/admin/Admin.php
@@ -2,18 +2,24 @@
 
 namespace App\Models\admin;
 
+use App\Helpers\Helper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, LogsActivity;
 
-    public static $admin='admin',$sub_admin='sub-admin';
+    public static $admin = 'admin', $sub_admin = 'sub-admin';
 
     protected $fillable = [
         'name',
@@ -47,12 +53,25 @@ class Admin extends Authenticatable
     public static function boot()
     {
         parent::boot();
-            static::deleting(function ($item){
-                $item->syncRoles([]);
-                $item->syncPermissions();
-            });
+        static::deleting(function ($item) {
+            $item->syncRoles([]);
+            $item->syncPermissions();
+        });
     }
-    
+
+    // Activity log using model
+    public function getActivitylogOptions(): LogOptions
+    {
+       
+        return LogOptions::defaults()->logAll();
+        // Chain fluent methods for configuration options
+    }
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+
+        $activity->causer_type = get_class(Auth::guard(Helper::getGuard())->user());
+        $activity->causer_id = Helper::getUserId();
+    }
     // get first name and last name seperatally
     public function getFirstnameAttribute()
     {
@@ -74,4 +93,3 @@ class Admin extends Authenticatable
         return $this->hasOne(AdminProfile::class, 'admin_id');
     }
 }
-
