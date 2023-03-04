c307c0cb0c36fe2781e2b12e533c1ec27fff31d9
shivam kumar <shivambaranwal790505@gmail.com>
2023-03-04 11:34:04 +0530

    api work

--- app/Models/GuardDuty.php
+++ app/Models/GuardDuty.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\customer\Property;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,4 +11,9 @@ class GuardDuty extends Model
 {
     use HasFactory,SoftDeletes;
     protected $guarded=[];
+
+    public function properties()
+    {
+        return $this->belongsTo(Property::class, 'property_id');
+    }
 }
--- routes/v1/security-guard.php
+++ routes/v1/security-guard.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\ProfileController;
 use App\Http\Controllers\v1\ApiAuthController;
+use App\Http\Controllers\v1\guard\ProfileController as GuardProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +19,19 @@ use Illuminate\Support\Facades\Route;
 
 
 // Auth login for guard and property owner
-Route::post('login', [ApiAuthController::class,'guard']);
+Route::post('login', [ApiAuthController::class, 'guard']);
 Route::get('user', [ApiAuthController::class, 'logged_in']);
 Route::get('logout', [ApiAuthController::class, 'logged_out']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('leave-guard', LeaveController::class)->middleware('can:leave');
+    Route::post('profile-image',[GuardProfileController::class,'profile_image'])->middleware('can:profile');
+    Route::post('update-profile',[GuardProfileController::class,'update_profile'])->middleware('can:profile');
+    Route::post('city',[GuardProfileController::class,'city'])->middleware('can:profile');
+    Route::post('country',[GuardProfileController::class,'country'])->middleware('can:profile');
+    Route::post('state',[GuardProfileController::class,'state'])->middleware('can:profile');
+    Route::post('change-password',[GuardProfileController::class,'change_password'])->middleware('can:profile');
+    Route::post('guard-properties',[GuardProfileController::class,'guard_properties'])->middleware('can:profile');
+    Route::post('guard-properties-details',[GuardProfileController::class,'guard_properties_details'])->middleware('can:profile');
+    Route::post('guard-properties-checkpoints',[GuardProfileController::class,'guard_properties_checkpoints'])->middleware('can:profile');
+});
 
